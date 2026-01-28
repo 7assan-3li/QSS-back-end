c40ddcbb70fb6d48d4f2565fbcb7f368621fd05f
7assan 3li <7.a.alm.3@gmail.com>
2026-01-28 23:06:24 +0300

    add resend verification email to api json file

--- routes/api/services.php
+++ routes/api/services.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 //service routes
 Route::get('/all-services', [ServiceController::class, 'showAll']);
 Route::middleware(['auth:sanctum', 'verified','seeker.policy'])->group(function () {
-
+Route::get('/services', [ServiceController::class, 'index']);
 Route::post('/services', [ServiceController::class, 'store']);
 Route::get('/services/{service}', [ServiceController::class, 'show']);
 Route::put('/services/{service}', [ServiceController::class, 'update']);
